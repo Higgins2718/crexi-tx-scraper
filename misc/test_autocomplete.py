26016b0fccdb26374acf7b2dde78d9950ffc9c2f
Tom Higgins <higgins2718@gmail.com>
2025-12-31 18:16:51 -0500

    remove ignored folders from repo

--- misc/test_autocomplete.py
+++
@@ -1,6 +0,0 @@
-import requests, urllib.parse, json
-addr = "7-Timberview-Rd-Lemont-IL-60439"
-url  = f"https://www.zillowstatic.com/autocomplete/v3/suggestions?q={urllib.parse.quote_plus(addr)}"
-zpid = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}).json()["results"][0]["metaData"]["zpid"]
-print(zpid)
-
